1660d5be7acb794738dcc74d98e5c01cd612664b
Andrei Vaniukov <aovnkv@yahoo.com>
2018-07-19 17:48:42 +0300

    feat: add new direction for enemy movement animation

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 // Enemies our player must avoid
-var Enemy = function(x, y) {
+var Enemy = function(x, y, toleft) {
   // Variables applied to each of our instances go here,
   // we've provided one for you to get started
-
   // The image/sprite for our enemies, this uses
   // a helper we've provided to easily load images
   this.sprite = 'images/enemy-bug.png';
+  this.toleft = toleft; //argument that points that enemy will move in an opposite direction
+  //ensure to load flipped image for this case
+  if (toleft) {
+    this.sprite = 'images/enemy-bug-flipped.png';
+  }
   this.x = x;
   this.y = y;
 };
@@ -16,16 +20,20 @@ Enemy.prototype.update = function(dt) {
   // You should multiply any movement by the dt parameter
   // which will ensure the game runs at the same speed for
   // all computers.
-  this.x += 150 * dt;
 
-  if (this.x > 505) {
-    this.x = -100;
+  //build the appropriate animation direction
+  !this.toleft ? (this.x += 150 * dt) : (this.x -= 150 * dt);
+
+  //ensure that enemy sprite constantly crosses the canvas
+  if (!this.toleft) {
+    if (this.x > 505) {
+      this.x = -100;
+    }
+  } else {
+    if (this.x < -70) {
+      this.x = 605;
+    }
   }
-  //collision check and player sprite position reset
-  //   if (this.x === player.x && this.y === player.y) {
-  //     player.x = 202;
-  //     player.y = 400;
-  //   }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -48,12 +56,14 @@ class Player {
   }
 
   update() {
+    //player's first move handling
     if (movex != undefined) {
       this.x = movex;
     }
     if (movey != undefined) {
       this.y = movey;
     }
+    //collision check and player sprite position reset
     for (let enemy of allEnemies) {
       if (
         this.y < enemy.y + 50 &&
@@ -90,7 +100,7 @@ class Player {
 // Place the player object in a variable called player
 
 const player = new Player(202, 400);
-const enemy1 = new Enemy(0, 60);
+const enemy1 = new Enemy(0, 60, true);
 const enemy2 = new Enemy(101, 145);
 const enemy3 = new Enemy(202, 234);
 const enemy4 = new Enemy();
@@ -100,18 +110,6 @@ const allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6];
 
 let movex;
 let movey;
-
-// allEnemies.forEach(function(enemy, i) {
-//   console.log('Enemy: ' + enemy + ' x: ' + enemy.x + ' y: ' + enemy.y);
-//   if (
-//     this.y < enemy.y + 50 &&
-//     this.y + 50 > enemy.y &&
-//     this.x < enemy.x + 50 &&
-//     this.x + 50 > enemy.x
-//   ) {
-//     player.y = 400;
-//   }
-// });
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
